fc722f0008c33af5157aca5d3394a0f3ff20aae3
Mário Ponte <ponteolavo30@gmail.com>
2022-05-11 20:51:42 +0100

    adicionar arquivos do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //const message = "Hello friend!";
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Comprar tomates',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Comprar batatas camponesas',
+      completed: true,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        < Tasks tasks={tasks}/>
+      </div>
+    </>
   );
 }
 
